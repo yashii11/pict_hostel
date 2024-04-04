7d839f7218daf4efe000daa687bc329106709353
yashii11 <136888879+yashii11@users.noreply.github.com>
2024-04-04 20:12:05 +0530

    notice mess logout features added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,30 @@ const path=require("path");
 const app=express();
 const hbs=require("hbs");
 const warden=require("./models/warden");
+const multer=require("multer");
+const session=require("express-session");
+
+const storage1 = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './src/uploads/notice');
+    },
+    filename: function(req, file, cb) {
+        cb(null, "notice.pdf");
+    },
+});
+
+const storage2 = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './src/uploads/menu');
+    },
+    filename: function(req, file, cb) {
+        cb(null, "menu.pdf");
+    },
+});
+
+
+const uploadNotice=multer({storage:storage1});
+const uploadMenu=multer({storage:storage2});
 
 require("./db/conn");
 const Register=require("./models/registers");
@@ -17,6 +41,26 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(static_path));
+
+app.use(session(
+    {secret: 'supersecretestring',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { 
+    //     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    //     maxAge: 7 * 24 * 60 * 60 * 1000,
+    //     httpOnly:true,
+    // },
+}));
+
+app.use((req,res,next)=>{
+    if(req.session.flash){
+        res.locals.flash=req.session.flash;
+        delete req.session.flash;
+    }
+    next();
+})
+
 app.set("view engine","hbs");
 app.set("views",view_path)
 app.get("/",(req,res)=>{
@@ -24,7 +68,7 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/login",(req,res)=>{
-    res.render("login");
+    res.render("login",{flash: res.locals.flash});
 })
 
 app.get("/register",(req,res)=>{
@@ -38,7 +82,23 @@ app.post("/register", async (req,res)=>{
         const password=req.body.password;
         const cpassword=req.body.confirmpassword;
         if(password===cpassword){
-            const registerStudent=new Register(req.body);
+            const registerStudent=new Register({
+                fname: req.body.fname,
+                lname:req.body.lname,
+                email:req.body.email,
+                password:req.body.password,
+                confirmpassword:req.body.confirmpassword,
+                gender:req.body.gender,
+                phone:req.body.phone,
+                address:req.body.address,
+                dob:req.body.dob,
+                yog:req.body.yearOfGrad,
+                bloodgroup:req.body.bloodgroup,
+                registerID:req.body.registerID,
+                state:req.body.state
+            })
+
+            const registered=await registerStudent.save();
             res.status(201).render("login")
         } else{
             res.send("Passwords are not matching!!");
@@ -55,14 +115,18 @@ app.post("/register", async (req,res)=>{
 app.post("/login",async (req,res)=>{
     try{
         const email=req.body.email;
+        req.session.mail=email;
         const password=req.body.password;
         console.log(`email:${email} and password: ${password}`);
         const useracc=await Register.findOne({email:email});
 
         if(useracc.password===password){
             res.status(201).render("student",{user:useracc});
+            req.session.flash={type:'success',message:'Login Successful!'};
         }else{
-            res.send("invalid login details");
+            req.session.flash={type:'error',message:'Invalid login credentials!'}
+            // res.send("invalid login details");
+            res.redirect('/');
         }
 
         // res.send(useracc);
@@ -72,6 +136,7 @@ app.post("/login",async (req,res)=>{
         console.log(error);
     }
 })
+
 
 app.get('/staff',async (req,res)=>
 {
@@ -103,6 +168,41 @@ app.post('/staff',async(req,res)=>
 
     }
 })
+
+app.get("/profile", async (req,res)=>{
+    
+    const useracc=await Register.findOne({email:req.session.mail});
+
+    res.status(201).render("profile",{user:useracc});
+})
+
+app.post('/uploadNotice',uploadNotice.single("notice"),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    return res.render("warden");
+});
+
+app.post('/uploadMenu',uploadMenu.single("menu"),(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    return res.render("warden");
+});
+
+app.get('/guidelines',(req,res)=>{
+    res.render("guidelines");
+})
+
+app.get('/logout',(req,res)=>{
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        // Redirect the user to a different page after logout
+        res.redirect('/');
+    });
+})
+
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 })
